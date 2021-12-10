018408d33b207932a2ac4eb366933f90137dfd52
rohitasva <rohitasva@gmail.com>
2021-12-09 17:30:15 -0800

    Fixed bad config

--- deploy.sh
+++ deploy.sh
@@ -20,7 +20,7 @@ curl --request POST \
           "environment": "'"$ENVIRONMENT"'",
           "application": "'"$CIRCLE_PROJECT_REPONAME"'",
           "source": "Github",
-          "status": "Failed",
+          "status": "Success",
           "startTime": "'"$START_TIME"'",
           "endTime": "'"$END_TIME"'"
         },
